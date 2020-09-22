a8f6e707a78668033fa0aa2c922c1942c85a7fdc
Biswadeep patra <42343581+BiswadeepPatra@users.noreply.github.com>
2020-09-22 23:40:36 +0530

    added row)num

--- spark_in_local/group_by_vs_window.py
+++ spark_in_local/group_by_vs_window.py
@@ -15,12 +15,16 @@ if __name__=="__main__":
     #spark.sql("select * from my_table ").show(10)
     lst=[(1,"sales",4200),(1,"admin",3100),(3,"sales",4000),(4,"sales",4000),(5,"admin",2700),(6,"dev",3400),(7,"dev",5200),(8,"dev",3700),(9,"dev",4400),(10,"dev",4400)]
     df=spark.createDataFrame(lst,["id","dept","salary"])
-    df.show()
-    df_new=df.groupBy("dept").agg(F.expr("avg(salary)").alias("average_salary"),F.expr("sum(salary)").alias("total_salary"))
-    df_new.show()
+    # df.show()
+    # df_new=df.groupBy("dept").agg(F.expr("avg(salary)").alias("average_salary"),F.expr("sum(salary)").alias("total_salary"))
+    # df_new.show()
     window_spec=Window.partitionBy("dept").orderBy(F.desc("salary"))
-    df_wind=df.withColumn("average_sal",F.avg(F.col("salary")).over(window_spec)).withColumn("total_salary",F.sum(F.col("salary")).over(window_spec))
-    df_wind.show()
+    # df_wind=df.withColumn("average_sal",F.avg(F.col("salary")).over(window_spec)).withColumn("total_salary",F.sum(F.col("salary")).over(window_spec))
+    # df_wind.show()
+
+    df.withColumn("rank",F.dense_rank().over(window_spec))\
+     .withColumn("rank",F.rank().over(window_spec))\
+     .withColumn("denserank",F.dense_rank().over(window_spec)).show()
     #
     # +---+-----+------+
     # | id | dept | salary |
